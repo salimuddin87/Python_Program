c0e465e162283779b76f0cbdcac20da092cb9373
salimkabeer <salimkabeer@gmail.com>
2019-01-31 00:05:45 +0530

    iterate dict

---
+++ IterateDictionary.py
@@ -0,0 +1,31 @@
+# Program to iterate dictionary
+
+class IterateDictionary():
+
+    def __init__(self):
+        self.initDict = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e'}
+
+    def defaultMethod(self):
+        print "By default dict iterate only keys"
+        for key in self.initDict:
+            print "%d -> %s" % (key, self.initDict[key])
+        return True
+    
+    def iterateValues(self):
+        print "Print Only Values of Dictionary"
+        for value in self.initDict.values():
+            print value
+        return True
+
+    def iterateKeysValues(self):
+        print "Iterate key, value"
+        for key,value in self.initDict.iteritems():
+            print key, value
+        return True
+
+
+if __name__ == '__main__':
+    obj1 = IterateDictionary()
+    obj1.defaultMethod()
+    obj1.iterateValues()
+    obj1.iterateKeysValues()
