319b97a408e4a6de2e42b55b05a5ae09a6006e9d
Md Salim <salimkabeer@gmail.com>
2019-03-26 08:20:18 +0530

    Create splittingAndSlicing.py

---
+++ splittingAndSlicing.py
@@ -0,0 +1,13 @@
+>>> stdout = "salim \n kabeer \n ansari \n year \n typeerror"
+>>> print stdout
+salim 
+ kabeer 
+ ansari 
+ year 
+ typeerror
+>>> x = stdout.split('\n')
+>>> print x
+['salim ', ' kabeer ', ' ansari ', ' year ', ' typeerror']
+>>> y = stdout.split('\n')[1:][:-1]
+>>> print y
+[' kabeer ', ' ansari ', ' year ']
