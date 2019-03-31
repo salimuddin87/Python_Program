4f205787965442cd137eb29734e8e42187c2e641
Md Salim <salimkabeer@gmail.com>
2019-03-31 09:39:27 +0530

    Update oop_program1.py

--- oopsProgram/oop_program1.py
+++ oopsProgram/oop_program1.py
@@ -23,13 +23,23 @@ emp1.displayEmployee()
 emp2.displayEmployee()
 print "Total Employee %d" % Employee.empCount
 """
-Instead of using the normal statements to access attributes, you can use the following functions −
-
+Instead of using the normal statements to access attributes, you can use the following functions
     - The getattr(obj, name[, default]) − to access the attribute of object.
-
     - The hasattr(obj,name) − to check if an attribute exists or not.
-
-    - The setattr(obj,name,value) − to set an attribute. If attribute does not exist, then it would be created.
-
+    - The setattr(obj,name,value) − to set an attribute. If attribute does not exist, 
+      then it would be created.
     - The delattr(obj, name) − to delete an attribute.
+    
+Built-In Class Attributes
+  - Every Python class keeps following built-in attributes and they can be accessed
+      using dot operator like any other attribute −
+
+    * __dict__ − Dictionary containing the class's namespace.
+    * __doc__ − Class documentation string or none, if undefined.
+    * __name__ − Class name.
+    * __module__ − Module name in which the class is defined. 
+      This attribute is "__main__" in interactive mode.
+    * __bases__ − A possibly empty tuple containing the base classes, 
+      in the order of their occurrence in the base class list.
+    
 """
