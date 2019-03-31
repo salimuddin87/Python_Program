af5871ba581f9e0e8b56835754e4e9927806a687
Md Salim <salimkabeer@gmail.com>
2019-03-31 09:30:40 +0530

    Update oop_program1.py

--- oopsProgram/oop_program1.py
+++ oopsProgram/oop_program1.py
@@ -22,3 +22,14 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print "Total Employee %d" % Employee.empCount
+"""
+Instead of using the normal statements to access attributes, you can use the following functions −
+
+    The getattr(obj, name[, default]) − to access the attribute of object.
+
+    The hasattr(obj,name) − to check if an attribute exists or not.
+
+    The setattr(obj,name,value) − to set an attribute. If attribute does not exist, then it would be created.
+
+    The delattr(obj, name) − to delete an attribute.
+"""
