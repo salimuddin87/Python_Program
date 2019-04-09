43ac4bfff094f313c5d5aa1a85269ed38d015630
salimkabeer <salimkabeer@gmail.com>
2019-04-10 00:23:46 +0530

    hahaha

--- keywordArguments.py
+++ keywordArguments.py
@@ -1,3 +1,18 @@
+"""
+--Do you have any Limburger ?
+--I am sorry, we are all out of Limburger
+**************************************************
+It's very runny, sir.
+##################################################
+It's really very, VERY runny, sir.
+##################################################
+client : John Cleese
+shopkeeper : Michael Palin
+sketch : Cheese Shop Sketch
+Data type of arguments : <type 'tuple'>
+Data type of keywords : <type 'dict'>
+
+"""
 
 def cheeseshop(kind, *arguments, **keywords):
     print "--Do you have any", kind, "?"
@@ -12,6 +27,7 @@ def cheeseshop(kind, *arguments, **keywords):
     for kw in keys:
         print kw, ":", keywords[kw]
 
+    print "*" * 50
     print "Data type of arguments :", type(arguments)
     print "Data type of keywords :", type(keywords)
 
--- problem/problem7_minWaterCan.py
+++ problem/problem7_minWaterCan.py
@@ -4,8 +4,29 @@ and they contain Used=[3,2,1,3,1]. The first can is full but 2nd
 can have 3 unit of space and so on.
 
 Return the min number of can required to carry "Used" water.
-The min number of can needed is 2 in this case.
+The min number of can needed is 2 (i.e. [0,5,0,5,0]) in this case.
 
 """
+def minCanNeeded(T, U):
+    minCan = 0
+    T.sort()
+    T = T[: : -1]
+    U_total = sum(U)
+    for i in range(0, len(T)):
+        if U_total == 0:
+            break
+        if U_total < T[i]:
+            minCan += 1
+            break
+        U_total -= T[i]
+        minCan += 1
+    return minCan
 
 
+if __name__ == '__main__':
+    T = [3,5,3,5,3]
+    # U = [3,2,1,3,1]
+    U = [3,2,1,3,3]
+
+    res = minCanNeeded(T, U)
+    print "Min can required : ", res
