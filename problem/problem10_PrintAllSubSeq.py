b9432d8ee28e27a8fb8987cf8b1c126aebb20780
sa186063 <sa186063@teradata.com>
2019-05-12 00:58:47 -0700

    edited sub seq problem

--- problem/problem10_PrintAllSubSeq.py
+++ problem/problem10_PrintAllSubSeq.py
@@ -1,5 +1,30 @@
 arr = [10,1,6,34,2,5,78,8,3,99,9]
 
+def findList(arr, item):
+    res = []
+    prevItem = item - 1
+    nextItem = item + 1
+    while prevItem in arr and prevItem not in res:
+        res.append(prevItem)
+        prevItem -= 1
+    while nextItem in arr and nextItem not in res:
+        res.append(nextItem)
+        nextItem += 1
+    return res
+
+def printAllSubseq(arr):
+    resDict = {}
+    key = 0
+    for item in arr:
+        key += 1
+        resDict[key] = [item]
+        arr.remove(item)
+        list1 = findList(arr, item)
+        resDict[key].extend(list1)
+        for item in list1:
+            arr.remove(item)
+    return resDict
+
 def printLargeSub(arr):
     arr.sort()
     for i in range(0, len(arr)):
@@ -14,4 +39,6 @@ def printLargeSub(arr):
 
 if __name__ == '__main__':
     
-    printLargeSub(arr)
+    outDict = printAllSubseq(arr)
+    for key in outDict.keys():
+        print outDict[key]
