bec352ca1240421fb7e982e1b1e032fef664c298
salimkabeer <salimkabeer@gmail.com>
2019-07-18 21:04:02 -0700

    kadane's algorithm

--- problem/problem17_maxSumSubset.py
+++ problem/problem17_maxSumSubset.py
@@ -1,8 +1,18 @@
 # Maximum sum of a subset of a given array
 
+# Kadane's Algorithm
 def maxSumSubset(arr):
-	pass
+	max_so_far = 0
+	max_end_here = 0
+	for i in range(len(arr)):
+		max_end_here += arr[i]
+		if max_end_here < 0:
+			max_end_here = 0
+		if max_so_far < max_end_here:
+			max_so_far = max_end_here
+
+	return max_so_far
 
 if __name__ == '__main__':
-	arr = [-2,1,3,-5,4,2]
-	maxSumSubset(arr)
+	arr = [-2, 1, -3, 4, -1, 3, 2, -5, 4]
+	print(maxSumSubset(arr))
