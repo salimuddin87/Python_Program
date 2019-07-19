a6afc7fab967cece1ba626979d6b78f9df9f1637
salimkabeer <salimkabeer@gmail.com>
2019-07-19 04:43:32 -0700

    max sum subset of an array

--- problem/problem17_maxSumSubset.py
+++ problem/problem17_maxSumSubset.py
@@ -1,18 +1,41 @@
 # Maximum sum of a subset of a given array
+from sys import maxsize
 
 # Kadane's Algorithm
 def maxSumSubset(arr):
-	max_so_far = 0
+	max_so_far = -maxsize - 1
 	max_end_here = 0
 	for i in range(len(arr)):
 		max_end_here += arr[i]
 		if max_end_here < 0:
 			max_end_here = 0
-		if max_so_far < max_end_here:
+		elif max_so_far < max_end_here:
 			max_so_far = max_end_here
 
 	return max_so_far
 
+# Print the max sub-array in a given array
+def printMaxSumSubset(arr):
+	max_so_far = -maxsize - 1
+	max_end_here = 0
+	start = end = curr = 0
+	for i in range(len(arr)):
+		max_end_here += arr[i]
+		
+		if max_so_far < max_end_here:
+			max_so_far = max_end_here
+			start = curr
+			end = i
+
+		if max_end_here < 0:
+			curr = i + 1
+			max_end_here = 0
+			
+	return max_so_far, start, end
+
 if __name__ == '__main__':
-	arr = [-2, 1, -3, 4, -1, 3, 2, -5, 4]
+	#arr = [-2, 1, -3, 4, -1, 3, 2, -5, 4]
+	#arr = [-3,-5,-1,-9,-3,-2,-4]
+	arr = [-3,-5,-1,-9,-3,2,-4]
 	print(maxSumSubset(arr))
+	print(printMaxSumSubset(arr))
