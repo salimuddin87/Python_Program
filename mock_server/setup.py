1245715bf73492f936a8cfafe73ede4d9b6fa654
salimkabeer <salimkabeer@gmail.com>
2019-07-20 21:03:17 -0700

    py test

--- mock_server/setup.py
+++ mock_server/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     zip_safe=True,
-    name='todolist_mock_server',
+    name='mock_server',
     version='1.0',
-    author='Aqua Security',
-    author_email='automation.qa@aquasec.com',
+    author='Salim',
+    author_email='salimkabeer@gmail.com',
     packages=[
     ],
-    license='Aqua Security',
+    license='free ware',
     description='Mock To Do List Server',
     install_requires=[
         'Flask==1.0.2'
