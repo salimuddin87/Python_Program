86aae6ffc2a24277682ccb14dfa9309257cc5701
salimkabeer <salimkabeer@gmail.com>
2019-07-23 05:06:53 -0700

    basic

--- mathsProblem/gcd_of_two_number.py
+++ mathsProblem/gcd_of_two_number.py
@@ -1,11 +1,11 @@
 
 
-# Time complexity is O(sqrt(n))
+# Time complexity is O(log(n))
 def gcd(n, m):
 	if n < m:
 		n, m = m, n # swap n and m
-	if n % m == 0:
-		return m
+	if m == 0:
+		return n
 	while m > 0:
 		n = n % m
 		n, m = m, n # swap n and m
