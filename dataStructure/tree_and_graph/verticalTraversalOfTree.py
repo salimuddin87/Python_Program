f12521fcd0073f1b265f0fde93172afde198154d
salimkabeer <salimkabeer@gmail.com>
2019-08-04 04:54:21 -0700

    hi

---
+++ dataStructure/tree_and_graph/verticalTraversalOfTree.py
@@ -0,0 +1,26 @@
+"""
+Input:-
+                   (a)
+                  /   \
+               (b)    (c)
+              /   \      \
+            (d)   (e)     (f)
+
+Output:- [[d], [b], [a, e], [c], [f]]            
+"""
+class Node():
+	"""docstring for Node"""
+	def __init__(self, info):
+		#super(Node, self).__init__()
+		self.left = None
+		self.info = info
+		self.right = None
+
+
+def verticalTraversal(root):
+	pass
+
+def createTree():
+	pass
+
+if __name__ == "__main__":
