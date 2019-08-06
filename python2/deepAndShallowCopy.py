4518e9274503daf62bd3a27a139c995bf68b2e00
salimkabeer <salimkabeer@gmail.com>
2019-08-06 07:11:27 -0700

    h

--- python2/deepAndShallowCopy.py
+++ python2/deepAndShallowCopy.py
@@ -1,3 +1,14 @@
+"""
+The difference between shallow and deep copying is only relevant for compound objects
+(objects that contain other objects, like lists or class instances):
+
+A shallow copy constructs a new compound object and then (to the extent possible) 
+	inserts references into it to the objects found in the original.
+
+A deep copy constructs a new compound object and then, recursively, inserts copies
+	into it of the objects found in the original.
+"""
+
 >>> a = [1,3,2,[1,2],5]
 >>> b = a (by default shallow copy)
 >>> b
