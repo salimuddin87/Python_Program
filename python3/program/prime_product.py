fe92f740e5da5ceff4b842c2b01bbc99a4831535
salimuddin87 <salimuddin.ansari@yahoo.com>
2020-10-02 10:39:21 +0530

    prime product number

---
+++ python3/program/prime_product.py
@@ -0,0 +1,26 @@
+def is_prime(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    for i in range(2, int(num / 2)):
+        if num % i == 0:
+            return False
+    return True
+
+
+def prime_product(m):
+    for i in range(2, m):
+        if is_prime(i) and (m % i == 0):
+            temp = m / i
+            if is_prime(temp):
+                return True
+            else:
+                return False
+
+    return False
+
+
+if __name__ == '__main__':
+    m = int(input("Enter a integer number: "))
+    print(prime_product(m))
