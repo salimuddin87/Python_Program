56961de367b0022ddb5927f120e641286ae9590d
md_salimuddin_ansari <md.salimuddin.ansari>
2020-10-20 17:01:41 +0530

    copy and date time

--- python3/Tutorial/copy/deep_shallow_copy.py
+++ python3/Tutorial/copy/deep_shallow_copy.py
@@ -3,50 +3,39 @@ The difference between shallow and deep copying is only relevant for compound ob
 (objects that contain other objects, like lists or class instances):
 
 A shallow copy constructs a new compound object and then (to the extent possible)
-	inserts references into it to the objects found in the original.
+    inserts references into it to the objects found in the original.
 
 A deep copy constructs a new compound object and then, recursively, inserts copies
-	into it of the objects found in the original.
+    into it of the objects found in the original.
 """
+import copy
 
->>> a = [1,3,2,[1,2],5]
->>> b = a (by default shallow copy)
->>> b
-[1, 3, 2, [1, 2], 5]
->>> b[3].append(3)
->>> b  (pointing to same object)
-[1, 3, 2, [1, 2, 3], 5]
->>> a
-[1, 3, 2, [1, 2, 3], 5]
 
->>> import copy
->>> c = copy.deepcopy(a)
->>> c
-[1, 3, 2, [1, 2, 3], 5]
->>> c.append(6)
->>> c
-[1, 3, 2, [1, 2, 3], 5, 6]
->>> a
-[1, 3, 2, [1, 2, 3], 5]
->>> c[3].append(4)
->>> c
-[1, 3, 2, [1, 2, 3, 4], 5, 6]
->>> a
-[1, 3, 2, [1, 2, 3], 5]
->>>
->>>
->>>
->>> d = copy.copy(a)
->>> d
-[1, 3, 2, [1, 2, 3], 5]
->>> d.append(6)
->>> d
-[1, 3, 2, [1, 2, 3], 5, 6]
->>> a
-[1, 3, 2, [1, 2, 3], 5]
->>> d[3].append(4)
->>> d
-[1, 3, 2, [1, 2, 3, 4], 5, 6]
->>> a
-[1, 3, 2, [1, 2, 3, 4], 5]
+if __name__ == '__main__':
+    a = [1, 3, 2, [1, 2], 5]
+    b = a  # by default shallow copy i.e. [1, 3, 2, [1, 2], 5]
+    b[3].append(3)
+    print(b)  # pointing to same object i.e. [1, 3, 2, [1, 2, 3], 5]
+    print(a)  # [1, 3, 2, [1, 2, 3], 5]
 
+    c = copy.deepcopy(a)
+    print(c)  # [1, 3, 2, [1, 2, 3], 5]
+
+    c.append(6)
+    print(c)  # [1, 3, 2, [1, 2, 3], 5, 6]
+    print(a)  # [1, 3, 2, [1, 2, 3], 5]
+
+    c[3].append(4)
+    print(c)  # [1, 3, 2, [1, 2, 3, 4], 5, 6]
+    print(a)  # [1, 3, 2, [1, 2, 3], 5]
+
+    d = copy.copy(a)
+    print(d)  # [1, 3, 2, [1, 2, 3], 5]
+
+    d.append(6)
+    print(d)  # [1, 3, 2, [1, 2, 3], 5, 6]
+    print(a)  # [1, 3, 2, [1, 2, 3], 5]
+
+    d[3].append(4)
+    print(d)  # [1, 3, 2, [1, 2, 3, 4], 5, 6]
+    print(a)  # [1, 3, 2, [1, 2, 3, 4], 5]
--- python3/Tutorial/time/epoch_time.py
+++ python3/Tutorial/time/epoch_time.py
@@ -1,18 +1,15 @@
->>> import datetime
->>> unixtimestamp = 1507126064
->>> datetime.datetime.fromtimestamp(unixtimestamp).strftime('%Y-%m-%d %H:%M:%S')
-'2017-10-04 07:07:44'
+import datetime
+import time
 
 
->>> import datetime
->>> timestamp = datetime.datetime(2017, 12, 1, 0, 0).strftime('%s')
->>> print(timestamp)
-1512115200
+unixtimestamp = 1507126064
+dt = datetime.datetime.fromtimestamp(unixtimestamp).strftime('%Y-%m-%d %H:%M:%S')
+print(dt)  # '2017-10-04 07:07:44'
 
+timestamp = datetime.datetime(2017, 12, 1, 0, 0).strftime('%s')
+print(timestamp)  # 1512115200
 
->>> import time
->>> date_time = '29.08.2011 11:05:02'
->>> pattern = '%d.%m.%Y %H:%M:%S'
->>> epoch = int(time.mktime(time.strptime(date_time, pattern)))
->>> print epoch
-1314641102
+date_time = '29.08.2011 11:05:02'
+pattern = '%d.%m.%Y %H:%M:%S'
+epoch = int(time.mktime(time.strptime(date_time, pattern)))
+print(epoch)  # 1314641102
